4a601e62c45bdc9133f4081b5b29663bf8b76946
AGraveMistake <brad@dominioncyber.net>
2026-02-23 00:09:24 -0600

    Update config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -8,6 +8,7 @@ export default withMermaid({
   // Fixes the build crash caused by dead links in markdown files
   ignoreDeadLinks: true,
   head: [
+    ['link', { rel: 'icon', href: '/talon_hunt_framework_documentation/favicon.ico' }],
     ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],
     ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' }],
     [
